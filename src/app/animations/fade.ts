82a4aaaefbc29181c28a2d9495a21494c0481900
Oleh Husak <40184919+oleh19127@users.noreply.github.com>
2024-03-02 21:13:47 +0200

    add animation preloader for app

--- src/app/animations/fade.ts
+++ src/app/animations/fade.ts
@@ -2,7 +2,7 @@ import { animate, style, transition, trigger } from '@angular/animations';
 
 export const fade = trigger('routeAnimations', [
   transition('* <=> *', [
-    style({ opacity: 0 }), // Start hidden
-    animate('300ms ease-in-out', style({ opacity: 1 })), // Fade in
+    style({ opacity: 0 }),
+    animate('300ms ease-in-out', style({ opacity: 1 })),
   ]),
 ]);
