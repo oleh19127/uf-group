ad433514f05098af6ed76b9452d68c58bbd73575
Oleh Husak <40184919+oleh19127@users.noreply.github.com>
2024-03-03 15:07:45 +0200

    create about component

--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { IntroComponent } from '../../blocks/intro/intro.component';
+import { AboutComponent } from '../../blocks/about/about.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [IntroComponent],
+  imports: [IntroComponent, AboutComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
