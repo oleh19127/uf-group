b3f1bfed4df554b62f8cb376aa20249a4e657073
Oleh Husak <40184919+oleh19127@users.noreply.github.com>
2024-03-04 19:37:33 +0200

    create our-news component

--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
 import { IntroComponent } from '../../blocks/intro/intro.component';
 import { AboutComponent } from '../../blocks/about/about.component';
+import { ServicesComponent } from '../../blocks/services/services.component';
+import { OurNewsComponent } from '../../blocks/our-news/our-news.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [IntroComponent, AboutComponent],
+  imports: [
+    IntroComponent,
+    AboutComponent,
+    ServicesComponent,
+    OurNewsComponent,
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
