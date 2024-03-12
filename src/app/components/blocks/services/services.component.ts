82f368d2497972e1ccf8736795fd1e2329810013
oleh19127 <oleh19127@gmail.com>
2024-03-12 19:00:24 +0200

    update progressiveImageComponent

--- src/app/components/blocks/services/services.component.ts
+++ src/app/components/blocks/services/services.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-services',
   standalone: true,
-  imports: [],
+  imports: [NgOptimizedImage],
   templateUrl: './services.component.html',
   styleUrl: './services.component.scss',
 })
