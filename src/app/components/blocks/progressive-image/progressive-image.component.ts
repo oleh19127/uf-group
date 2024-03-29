52ed6548ba28ce978eeb7b0abb65c02795275dd7
oleh19127 <oleh19127@gmail.com>
2024-03-29 17:30:20 +0200

    test provideImgixLoader

--- src/app/components/blocks/progressive-image/progressive-image.component.ts
+++ src/app/components/blocks/progressive-image/progressive-image.component.ts
@@ -1,16 +1,22 @@
 import { booleanAttribute, Component, Input } from '@angular/core';
-import { NgClass, NgIf, NgOptimizedImage, NgStyle } from '@angular/common';
+import {
+  NgClass,
+  NgIf,
+  NgOptimizedImage,
+  NgStyle,
+  provideImgixLoader,
+} from '@angular/common';
 
 @Component({
   selector: 'app-progressive-image',
   standalone: true,
   imports: [NgStyle, NgClass, NgOptimizedImage, NgIf],
+  providers: [provideImgixLoader('https://oberon-985042972.imgix.net')],
   templateUrl: './progressive-image.component.html',
   styleUrl: './progressive-image.component.scss',
 })
 export class ProgressiveImageComponent {
   @Input({ required: true }) imageUrl!: string;
-  @Input({ required: true }) imageUrlPlaceholder!: string;
   @Input({ required: true }) imageAlt!: string;
   @Input({}) imageWidth!: string;
   @Input({}) imageHeight!: string;
@@ -18,8 +24,5 @@ export class ProgressiveImageComponent {
   imagePriority!: boolean;
   @Input({ transform: booleanAttribute, required: true }) imageFill!: boolean;
 
-  isLoaded = false;
-  onImageLoad() {
-    this.isLoaded = true;
-  }
+  imageSrcset = '640w, 750w, 828w, 1080w, 1200w, 1920w, 2048w, 3840w';
 }
