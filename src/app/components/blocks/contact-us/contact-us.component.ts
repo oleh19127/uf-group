3e284dffb08ff7eefba929ab54b53786232bc93f
oleh19127 <oleh19127@gmail.com>
2024-04-02 14:42:45 +0300

    update dark theme for some sections

--- src/app/components/blocks/contact-us/contact-us.component.ts
+++ src/app/components/blocks/contact-us/contact-us.component.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { ProgressiveImageComponent } from '../progressive-image/progressive-image.component';
+import { ThemeService } from '../../../services/theme.service';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-contact-us',
   standalone: true,
-  imports: [ProgressiveImageComponent],
+  imports: [ProgressiveImageComponent, NgClass],
   templateUrl: './contact-us.component.html',
   styleUrl: './contact-us.component.scss',
 })
-export class ContactUsComponent {}
+export class ContactUsComponent {
+  themeService: ThemeService = inject(ThemeService);
+}
